1ae8e6dcf21d0a3aa6ca8da573c0dfeb34ea7d2f
Eliud Ayala <73802657+Eliud20@users.noreply.github.com>
2021-11-18 17:39:44 -0600

    Add files via upload

---
+++ poke.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+
+function getPokebyId(id){
+    var url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    console.log(url);
+    request(url, function(error, response, body){
+        console.log(response.statusCode);
+        if(response.statusCode == 200){
+            let pokemon = JSON.parse(body);
+            console.log(pokemon.id);
+            for(let i=0; i<pokemon.abilities.length; i++){
+                console.log(pokemon.abilities[i].ability.name);
+            }
+            console.log(pokemon.name);
+        } else {
+            console.log("Error" + error);
+        }
+    })
+}
+
+
+
+getPokebyId(2);
