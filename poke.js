7cfcd940a14403609d6fcc184038283a8c60e819
Eliud Ayala <73802657+Eliud20@users.noreply.github.com>
2021-11-23 17:43:41 -0600

    Otra prueba

--- poke.js
+++ poke.js
@@ -1,26 +1,30 @@
-
-const request = require("request");
-const vec = [];
-
-function getPokebyId(id) {
-    var url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-    console.log(url);
-    var pok;
-    request(url, function (error, response, body) {
-        console.log(response.statusCode);
-        if (response.statusCode == 200) {
-            var pokemon = JSON.parse(body);
-            console.log(pokemon.name);
-            document.getElementById("nom1").innerHTML = (pokemon.name).JSON.parse;
-            for (let i = 0; i < pokemon.abilities.length; i++) {
-                vec[i] = pokemon.abilities[i].ability.name;
-                console.log(vec[i]);
-            }
-        } else {
-            console.log("Error" + error);
-        }
-        pok = pokemon;
+fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+    .then(res => res.json())
+    .then(data => {
+        ciclo(data);
     })
-    return pok;
+    .catch(error => console.log(error))
+
+function obtenerPoke(poke) {
+    let x = document.getElementById("tot");
+    let h = document.createElement("div")
+    h.className = "grid-item";
+    h.innerHTML = poke.name;
+    x.appendChild(h);
+    console.log(poke.name);
 }
 
+function ciclo(data) {
+    let vec = [];
+    for (let i = 1; i <= 151; i++) {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+            .then(res => res.json())
+            .then(data => {
+                //console.log(`Id: ${data.id} Nombre: ${data.name}`);
+                vec[i] = data.name, data.id;
+            })
+            .catch(error => console.log(error))
+    }
+    console.log(vec);
+
+}
