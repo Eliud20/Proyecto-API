64bd2bd4aa606d52703feb1300c9574954e770f2
Eliud Ayala <73802657+Eliud20@users.noreply.github.com>
2021-11-24 00:09:47 -0600

    Pruebas Linux

--- poke.js
+++ poke.js
@@ -1,30 +1,30 @@
-fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`)
-    .then(res => res.json())
-    .then(data => {
-        ciclo(data);
-    })
-    .catch(error => console.log(error))
+var prueb = [];
 
 function obtenerPoke(poke) {
     let x = document.getElementById("tot");
     let h = document.createElement("div")
+    let img = document.createElement('img');
+    let salto = document.createElement('br');
+    img.src = poke.sprites.front_default;
     h.className = "grid-item";
+    img.className = "ima";
     h.innerHTML = poke.name;
     x.appendChild(h);
+    h.appendChild(img);
+    img.appendChild(salto);
     console.log(poke.name);
 }
 
-function ciclo(data) {
-    let vec = [];
-    for (let i = 1; i <= 151; i++) {
+function pedirPok() {
+    for (let i = 1; i < 152; i++) {
         fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
             .then(res => res.json())
             .then(data => {
-                //console.log(`Id: ${data.id} Nombre: ${data.name}`);
-                vec[i] = data.name, data.id;
+                obtenerPoke(data);
             })
             .catch(error => console.log(error))
     }
-    console.log(vec);
-
 }
+pedirPok();
+console.log(prueb);
+
