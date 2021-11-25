3b1cbb5689629291c287e77b7a8fde66066e78df
Eliud Ayala <73802657+Eliud20@users.noreply.github.com>
2021-11-25 02:22:48 -0600

    Ultimos detalles

--- poke.js
+++ poke.js
@@ -1,10 +1,79 @@
 var prueb = [];
 var grid = document.getElementById("tot");
-var contenedor = document.querySelector('div');
+var contenedor = document.getElementsByClassName('grid-item');
+var id = 0;
+var modal = document.getElementById("mod");
+var btn = document.getElementById("boton");
+var span = document.getElementsByClassName("close")[0];
+var input = document.querySelector("input");
+var a = "";
 
-function obtenerPoke(poke) {
+
+btn.onclick = function () {
+    modal.style.display = "block";
+    let div = document.createElement("div");
+    div.innerHTML = buscarPoke(input.value);
+    console.log(buscarPoke(input.value));
+    modal.appendChild(div);
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function buscarPoke(poke) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`)
+        .then(res => res.json())
+        .then(data => {
+            a = data.name;
+            return a;
+        })
+        .catch(error => console.log(error))
+}
+
+
+function pedirPok() {
+    for (let i = 1; i <= 155; i++) {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+            .then(res => res.json())
+            .then(data => {
+                imprimirPoke(data);
+            })
+            .catch(error => console.log(error))
+    }
+}
+
+function capitalizeOnlyFirst(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getTypes(poke) {
+    let cad = "";
+    for (let i = 0; i < poke.types.length; i++) {
+        cad += poke.types[i].type.name + ", ";
+    }
+    return cad.slice(0, -2);
+}
+
+function imprimirPoke(poke) {
     let div = document.createElement("div")
     div.className = "grid-item";
+
+    let div2 = document.createElement("div");
+    div2.innerHTML = `Tipo(s): <br>${capitalizeOnlyFirst(getTypes(poke))}`;
+
+    let but = document.createElement('button');
+    but.className = "bot";
+    but.id = `${id}`;
+    id++;
 
     let img = document.createElement('img');
     img.src = poke.sprites.front_default;
@@ -12,28 +81,18 @@ function obtenerPoke(poke) {
 
     const nom = poke.name;
 
-    function capitalizeOnlyFirst(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-
     let salto = document.createElement('br');
-    div.innerHTML = `#${poke.id} <br>${capitalizeOnlyFirst(nom)}`;
+    div.innerHTML = `#${poke.id} <br>${capitalizeOnlyFirst(nom)}<br>`;
 
-    grid.appendChild(div);
+    grid.appendChild(but);
+    but.appendChild(div);
     div.appendChild(salto);
     div.appendChild(img);
     div.appendChild(salto);
+    div.appendChild(div2);
+
 }
 
-function pedirPok() {
-    for (let i = 1; i <= 153; i++) {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-            .then(res => res.json())
-            .then(data => {
-                obtenerPoke(data);
-            })
-            .catch(error => console.log(error))
-    }
-}
+
 pedirPok();
 console.log(prueb);
